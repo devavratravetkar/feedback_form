0424ff616aff31e9684332dc72b2942ed50bcebb
Devavrat Ravetkar <devavrat.ravetkar@yahoo.com>
2025-02-13 01:33:39 -0500

    Add basic form input elements

--- src/Components/FeedbackForm.jsx
+++ src/Components/FeedbackForm.jsx
@@ -5,14 +5,28 @@ const FeedbackForm = () => {
 
   return (
     <>
-    <nav>
-    Tell Us What You Think
-    </nav>
-      <form className="feedback-form">
-        <h2>We'd Love to Hear From You!</h2>
-        <p>Please share your feedback with us.</p>
-        
-      </form>
+        <nav>
+        Tell Us What You Think
+        </nav>
+        <form className="feedback-form">
+            <h2>We'd Love to Hear From You!</h2>
+            <p>Please share your feedback with us.</p>
+            <input
+            type="text"
+            name="name"
+            placeholder="Your Name"
+            />
+            <input
+            type="email"
+            name="email"
+            placeholder="Your Email"
+            />
+            <textarea
+            name="feedback"
+            placeholder="Your Feedback"
+            ></textarea>
+            <button type="submit">Submit Feedback</button>
+            </form>
     </>
   );
 };
